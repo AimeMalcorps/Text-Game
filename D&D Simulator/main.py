c684d2d2ccabe5771898ad2f1d81bf4fa8c1fa52
Aimé Malcorps <62387133+AimeMalcorps@users.noreply.github.com>
2020-11-13 12:05:33 +0100

    Update main.py

--- D&D Simulator/main.py
+++ D&D Simulator/main.py
@@ -13,13 +13,4 @@ keyboard.press_and_release('win + left arrow')
 time.sleep(0.1)
 os.system("start cmd C:/Users/Aimé/Desktop/Prog/Jeu Textuel/ /k py fiche_perso.py")
 
-carac_pero = ["Points de vie",20,"Endurance",3,"Force",8,"Chance",6,]
-pickle.dump(carac_pero,open('carac_pero.p','wb'))
-contenu_sac = ["Pèces d'or",5,"Tunique légère"," ","Epée médiocre"," ",]
-pickle.dump(contenu_sac,open('contenu_sac.p','wb'))
-quetes =["Allez voir le forgeron, il se trouve au marché."]
-pickle.dump(quetes,open('quetes.p','wb'))
-QuetesUpdate("Retrouvez le marteau du forgeron à l'Est de la ville.\n\n")
-
-
-combat("bandit",10,5)
+intro()
